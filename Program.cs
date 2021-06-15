7f88c31f23a6046c23b74d41f66c726f9f054b94
distorition <85959781+distorition@users.noreply.github.com>
2021-06-15 21:07:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Гик_Бреинс_курсы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ввидите имя");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Дарова {name} сегодня {DateTime.Now}");
+        }
+    }
+}
